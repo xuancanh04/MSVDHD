a51abbb0eb4ab68ac40aafda074ef86f71d7f137
xuancanh04 <161755747+xuancanh04@users.noreply.github.com>
2024-10-16 11:15:26 +0700

    Add files via upload

---
+++ HCMSIU/HCMSIU/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HCMSIU.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
